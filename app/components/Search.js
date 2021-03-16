afdfb29cd1438f8119e2fca1557b81fc4d094f38
Alexandra Langton <alexandra.langton@outlook.com>
2021-03-16 17:55:42 -0400

    Added toast nofications for error handling

--- app/components/Search.js
+++ app/components/Search.js
@@ -1,40 +1,49 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Search = ({ setProfile }) => {
 	const [url, setUrl] = useState('');
 
 	const getPreview = async (e) => {
+		e.preventDefault();
 		if (!url.match(/www.linkedin.com/i)) {
-			alert('Please enter a valid linkedin url');
+			toast.error('Please enter a valid LinkedIn url');
 		} else {
-			e.preventDefault();
 			let urlEnding = url
 				.replace(/((https?:\/\/)?www\.linkedin\.com(\/)?(in)?)(\/)/g, '')
 				.replace(/(\/)$/, '');
 			const res = await axios.get(`/api/preview/${urlEnding}`);
+			if (res.status !== 200) {
+				toast.error(res.data);
+			}
 			setProfile(res.data);
 		}
 		setUrl('');
 	};
 
 	return (
-		<div id="searchBox" className="whiteBox">
-			<form onSubmit={getPreview}>
-				<h1>LinkedIn Preview Card Generator</h1>
-				<p>Enter a valid LinkedIn url to fetch the respective preview:</p>
-				<input
-					id="searchbar"
-					name="url"
-					type="text"
-					value={url}
-					onChange={(e) => setUrl(e.target.value)}
-				/>
-				<button id="searchbutton" type="submit">
-					Submit
-				</button>
-			</form>
-		</div>
+		<>
+			<ToastContainer />
+			<div id="searchBox" className="whiteBox">
+				<form onSubmit={getPreview}>
+					<h1>LinkedIn Preview Card Generator</h1>
+					<p>Enter a valid LinkedIn url to fetch the respective preview:</p>
+					<input
+						id="searchbar"
+						name="url"
+						type="text"
+						value={url}
+						onChange={(e) => setUrl(e.target.value)}
+					/>
+					<button id="searchbutton" type="submit">
+						Submit
+					</button>
+					<br />
+				</form>
+			</div>
+		</>
 	);
 };
 
